bce2518c53cc44c1f64c4c3ea8bfa45bf6dbc3c0
Jaffe <jaffe75@gmail.com>
2013-08-06 13:38:27 -0700

    changing header stuff in master - oops

--- js/helpmakeitwork.js
+++ js/helpmakeitwork.js
@@ -1,11 +1,8 @@
 // JS
 jQuery(document).ready(function($){
+	// grab the top navigation
 	var nav = $('.top-bar-section > ul li');  
 	var navPos = $('.top-bar-section');
-	var menuItem = $('.menu-item');
-	menuItem.removeClass('active');
-	var current = $('.current-menu-item');
-	current.addClass('active');
 	
 	nav.before('<li class="divider"/>'); 
 
