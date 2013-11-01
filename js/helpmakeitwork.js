2d400ca0ed7c14b439960feb9df41ad90877c854
Jaffe <jaffe75@gmail.com>
2013-10-31 19:54:31 -0700

    Commenting out the js, Walker class taking over

--- js/helpmakeitwork.js
+++ js/helpmakeitwork.js
@@ -1,17 +1,17 @@
 // JS
 jQuery(document).ready(function($){
-	var nav = $('.top-bar-section > ul li');  
-	var navPos = $('.top-bar-section');
-	var menuItem = $('.menu-item');
-	menuItem.removeClass('active');
-	var current = $('.current-menu-item');
-	current.addClass('active');
+	// var nav = $('.top-bar-section > ul li');  
+	// var navPos = $('.top-bar-section');
+	// var menuItem = $('.menu-item');
+	// menuItem.removeClass('active');
+	// var current = $('.current-menu-item');
+	// current.addClass('active');
 	
-	nav.before('<li class="divider"/>'); 
+	// nav.before('<li class="divider"/>'); 
 
-	var subNav = $('.dropdown');
-	subNav.parent('li').addClass('has-dropdown');
-	subNav.addClass('dropdown');
+	// var subNav = $('.dropdown');
+	// subNav.parent('li').addClass('has-dropdown');
+	// subNav.addClass('dropdown');
 
 
 }); // annon
