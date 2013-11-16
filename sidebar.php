c4988f2f67aa9bcbd13635d035aa3eb58cc70947
Jaffe <jaffe75@gmail.com>
2013-11-16 16:14:00 -0700

    changing h1 tags to h3 tags

--- sidebar.php
+++ sidebar.php
@@ -14,14 +14,14 @@
 			</aside>
 
 			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'foundation_s' ); ?></h1>
+				<h3 class="widget-title"><?php _e( 'Archives', 'foundation_s' ); ?></h3>
 				<ul>
 					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 				</ul>
 			</aside>
 
 			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'foundation_s' ); ?></h1>
+				<h3 class="widget-title"><?php _e( 'Meta', 'foundation_s' ); ?></h3>
 				<ul>
 					<?php wp_register(); ?>
 					<li><?php wp_loginout(); ?></li>
